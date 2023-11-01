8bf46b0529f1a26de4bb6b5d5429970ce02f75b6
GONZALES <gonzalesfranz2019@gmail.com>
2023-11-01 11:11:55 -0400

    C-7

--- Grupo-7 Sistema_Gestion_Medicos_Especialidades/class/medico.js
+++ Grupo-7 Sistema_Gestion_Medicos_Especialidades/class/medico.js
@@ -1,9 +1,10 @@
 
 class Medico{
-    constructor(id, foto, nombre, email, password, descripcion,educacion, direccion, hr_disponible, id_especialidad){
+    constructor(id, foto, nombre, apellido, email, password, descripcion,educacion, direccion, hr_disponible, id_especialidad){
         this.id = id;
         this.foto = foto;
         this.nombre = nombre;
+        this.apellido = apellido;
         this.email = email;
         this.password = password;
         this.descripcion = descripcion;
@@ -36,12 +37,13 @@ class ControlMedico{
         return this.listaMedicos;
     }
 
-    editar_medico(id, nv_foto, nv_nombre, nv_email, nv_password, nv_esperiencia, nv_educacion, nv_direccion, nv_hr_disponible, nv_id_especialidad){
+    editar_medico(id, nv_foto, nv_nombre, nv_apellido, nv_email, nv_password, nv_esperiencia, nv_educacion, nv_direccion, nv_hr_disponible, nv_id_especialidad){
         const medicoAModificar = this.listaMedicos.find((medico) => medico.id === id);
 
         if(medicoAModificar){
             medicoAModificar.foto = nv_foto;
             medicoAModificar.nombre = nv_nombre;
+            medicoAModificar.apellido = nv_apellido;
             medicoAModificar.email = nv_email;
             medicoAModificar.password = nv_password;
             medicoAModificar.descripcion = nv_descripcion;
@@ -79,14 +81,12 @@ class ControlMedico{
 
     listar_medicos(){
         let medicos = [
-            [0, "foto", "Juan", "juanito@gmail.com", "123", "Cofundador, socio, y Global Business Development Manager MD con MBA, especializado en nuevos negocios e innovación en el área de la salud. Tiene experiencia en compañías aseguradoras, así como en gestión clínica y sanitaria, y está interesado en el turismo médico, la innovación en dispositivos médicos, y nuevos servicios TIC de salud Cofundador de la startup Doctoralia, ahora parte del Grupo Docplanner, la plataforma líder global que pone en contacto a pacientes y especialistas. Además, asesora otros proyectos digitales desde Barcelona Health Hub con Braincats Consulting, como el e-commerce médico Clinicpoint, el servicio de IA Universal Customer, o el servicio de telemedicina y control de pacientes crónicos Doctivi", "USFX", "Calle Loa Nr: 122", "10:00am a 12:00pm", 1],
-            
-            
-            [1, "foto", "Pedro", "pedro@gmail.com", "1a2", "Radioterapia", "USFX", "Calle Loa Nr: 122", "10:00am a 12:00pm", 1],
-            [2, "foto", "Lucas", "lucas@gmail.com", "afed", "Radioterapia", "USFX", "Calle Loa Nr: 122", "10:00am a 12:00pm", 1],
-            [3, "foto", "Margarita", "margarita@gmail.com", "120fr3", "Radioterapia", "USFX", "Calle Loa Nr: 122", "10:00am a 12:00pm", 1],
-            [4, " Fotos", "Elena", "elena@gmail.com", "dewf", "Radioterapia", "USFX", "Calle Loa Nr: 122", "10:00am a 12:00pm", 1],
-            [5, "foto", "Franz", "franz@gmail.com", "12sasd", "Radioterapia", "USFX", "Calle Loa Nr: 122", "10:00am a 12:00pm", 1]
+            [0, "foto", "Juan", "Perez", "juanito@gmail.com", "123", "Cofundador, socio, y Global Business Development Manager MD con MBA, especializado en nuevos negocios e innovación en el área de la salud. Tiene experiencia en compañías aseguradoras, así como en gestión clínica y sanitaria, y está interesado en el turismo médico, la innovación en dispositivos médicos, y nuevos servicios TIC de salud Cofundador de la startup Doctoralia, ahora parte del Grupo Docplanner, la plataforma líder global que pone en contacto a pacientes y especialistas. Además, asesora otros proyectos digitales desde Barcelona Health Hub con Braincats Consulting, como el e-commerce médico Clinicpoint, el servicio de IA Universal Customer, o el servicio de telemedicina y control de pacientes crónicos Doctivi", "USFX", "Calle Loa Nr: 122", "10:00am a 12:00pm", 1],
+            [1, "foto", "Pedro", "Castro", "pedro@gmail.com", "1a2", "Radioterapia", "USFX", "Calle Loa Nr: 122", "10:00am a 12:00pm", 1],
+            [2, "foto", "Lucas", "Martinez", "lucas@gmail.com", "afed", "Radioterapia", "USFX", "Calle Loa Nr: 122", "10:00am a 12:00pm", 1],
+            [3, "foto", "Margarita", "Flores", "margarita@gmail.com", "120fr3", "Radioterapia", "USFX", "Calle Loa Nr: 122", "10:00am a 12:00pm", 1],
+            [4, " Fotos", "Elena", "Rios", "elena@gmail.com", "dewf", "Radioterapia", "USFX", "Calle Loa Nr: 122", "10:00am a 12:00pm", 1],
+            [5, "foto", "Franz", "Gonzales", "franz@gmail.com", "12sasd", "Radioterapia", "USFX", "Calle Loa Nr: 122", "10:00am a 12:00pm", 1]
         ];
 
         for (let i = 0; i < medicos.length; i++) {
